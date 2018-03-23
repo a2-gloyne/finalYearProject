9a395706c2570efb287edb836a6dc087d3f87aeb
a2-gloyne <33129085+a2-gloyne@users.noreply.github.com>
2018-03-23 21:23:32 +0000

    Add files via upload

---
+++ src/main/java/com/example/finalyearproject/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.example.finalyearproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText inputEmail, inputPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        inputEmail = findViewById(R.id.email);
+        inputPassword = findViewById(R.id.password);
+    }
+
+    public void login(View v) {
+        int buttonID;
+
+    }
+
+    public void register(View v) {
+        int buttonID;
+
+    }
+}
